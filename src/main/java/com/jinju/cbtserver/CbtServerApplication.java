ce52d8a45f0793754aa49c2941922dd68f1be301
jinju9513 <wjswlswn99@naver.com>
2025-11-13 11:31:19 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/jinju/cbtserver/CbtServerApplication.java
@@ -0,0 +1,13 @@
+package com.jinju.cbtserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbtServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbtServerApplication.class, args);
+    }
+
+}
